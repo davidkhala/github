931e6d021eeb877e85e718c1160e601b93592bc8
David Liu <david-khala@hotmail.com>
2024-10-07 12:22:02 +0800

    Update gcp.sh

--- enterprise/gcp.sh
+++ enterprise/gcp.sh
@@ -4,7 +4,12 @@
 # Memory: 32 GB
 # Storage: Root=200GB, Data=150GB
 
+image=github-enterprise-3-14-1
 
 
+list-images() {
+  gcloud compute images list --project github-enterprise-public --no-standard-images
+}
 
-gcloud compute images list --project github-enterprise-public --no-standard-images
+$@
+
