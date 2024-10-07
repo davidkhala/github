c7b08d87e195793e179849cd2cb00c1396bcd8f6
David Liu <david-khala@hotmail.com>
2024-10-07 12:20:18 +0800

    Create gcp.sh

---
+++ enterprise/gcp.sh
@@ -0,0 +1,10 @@
+
+# Purpose: Trial, demo, or 10 light users
+# vCPUs: 4
+# Memory: 32 GB
+# Storage: Root=200GB, Data=150GB
+
+
+
+
+gcloud compute images list --project github-enterprise-public --no-standard-images
