b3de40a4fe9132402c0502695e9db40da1704c64
David Liu <david-khala@hotmail.com>
2024-10-07 12:57:31 +0800

    Update gcp.sh

--- enterprise/gcp.sh
+++ enterprise/gcp.sh
@@ -1,15 +1,29 @@
-
+set -x
 # Purpose: Trial, demo, or 10 light users
 # vCPUs: 4
 # Memory: 32 GB
 # Storage: Root=200GB, Data=150GB
 
 image=github-enterprise-3-14-1
-
+instance=github-enterprise
+data_disk=ghe-data-disk
 
 list-images() {
   gcloud compute images list --project github-enterprise-public --no-standard-images
 }
+
+create(){
+  # Create data disk
+  gcloud compute disks create $data_disk --size=150GB --zone=${zone}
+  gcloud compute instances create $instance --machine-type e2-highmem-4 \
+    --image-project github-enterprise-public --image=$image \
+    --disk name=${data_disk} \
+    --metadata serial-port-enable=1 \
+    --network NETWORK-NAME \
+    --zone=${zone}
+   
+}
+
 
 $@
 
