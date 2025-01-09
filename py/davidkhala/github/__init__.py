f824312b689c59bc5296d66fbdd85f62c89f67d9
David Liu <david-khala@hotmail.com>
2025-01-10 02:10:27 +0800

    fixing CI

--- py/davidkhala/github/__init__.py
+++ py/davidkhala/github/__init__.py
@@ -1,9 +1,10 @@
 from github import Github as G
 from github.Auth import Token
+from github.AuthenticatedUser import AuthenticatedUser
 
 
 class Github:
-    def __init__(self, auth: Token=None, hostname= None):
+    def __init__(self, auth: Token = None, hostname=None):
         opts = {}
         if hostname:
             opts['hostname'] = f"https://{hostname}/api/v3"
@@ -13,3 +14,8 @@ class Github:
             **opts
         )
 
+    def me(self) -> AuthenticatedUser:
+        return self.client.get_user()
+
+    def disconnect(self):
+        self.client.close()
--- py/tests/rest_test.py
+++ py/tests/rest_test.py
@@ -5,14 +5,18 @@ from davidkhala.github import Github
 from davidkhala.github.auth import from_pat
 
 
-class AuthTestCase(unittest.TestCase):
-    def test_from_pat(self):
-        auth =from_pat(os.environ.get('GITHUB_TOKEN'))
-        print(auth)
-        g= Github(auth)
-        for repo in g.client.get_user().get_repos():
-            print(repo.owner.name, repo.name)
-        g.client.close()
+class PATTestCase(unittest.TestCase):
+
+    def setUp(self):
+        auth = from_pat(os.environ.get('GITHUB_TOKEN'))
+        print(auth.token)
+        self.g = Github(auth)
+
+    def test_self(self):
+        self.assertEqual(self.g.me().login, 'davidkhala')
+
+    def tearDown(self):
+        self.g.disconnect()
 
 if __name__ == '__main__':
     unittest.main()
